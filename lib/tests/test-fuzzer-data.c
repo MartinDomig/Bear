12ca015348c46f7249069d845f9cbca0544faf44
Martin Domig <martin.domig@wolfvision.net>
2024-07-28 15:03:38 +0200

    Add test case for broken block size generation

--- lib/tests/test-fuzzer-data.c
+++ lib/tests/test-fuzzer-data.c
@@ -49,6 +49,45 @@ static void static_hex() {
   g_assert_cmpmem(g_bytes_get_data(data, NULL), 5, expected, 5);
 }
 
+static void variable_block_size() {
+  g_autoptr(BearFuzzer) fuzzer = bear_fuzzer_new(NULL);
+  bear_fuzzer_block_size(fuzzer, "block", sizeof(guint8));
+
+  bear_fuzzer_begin_block(fuzzer, "block");
+  BearFuzzyValue *variable_value = bear_fuzzer_variable_string(fuzzer, "");
+  bear_fuzzy_value_set_strings(variable_value, "a", "bb", "ccc", NULL);
+  bear_fuzzer_end_block(fuzzer, "block");
+
+  gchar *vector = bear_fuzzer_get_start_vector(fuzzer);
+  GList *values = NULL;
+  while (vector) {
+    values = g_list_append(values, bear_fuzzer_get_data(fuzzer, vector));
+    gchar *new_vector = bear_fuzzer_get_next_vector(fuzzer, vector);
+    g_free(vector);
+    vector = new_vector;
+  }
+
+  g_assert_cmpuint(g_list_length(values), ==, 3);
+
+  guint8 **expected = (guint8 *[]){
+      (guint8[]){2, 1, 'a'},
+      (guint8[]){3, 2, 'b', 'b'},
+      (guint8[]){4, 3, 'c', 'c', 'c'},
+  };
+
+  for (guint i = 0; i < 3; i++) {
+    GBytes *data = g_list_nth_data(values, i);
+    g_autoptr(GBytes) expected_data = g_bytes_new(expected[i] + 1, expected[i][0]);
+    g_autofree gchar *expected_hex = g_strstrip(bear_tools_bytes_to_hex(expected_data));
+    g_autofree gchar *data_hex = g_strstrip(bear_tools_bytes_to_hex(data));
+    g_debug("Variable block #%u\nEXP: %s\nGOT: %s", i, expected_hex, data_hex);
+
+    g_assert_cmpstr(expected_hex, ==, data_hex);
+  }
+
+  g_list_free_full(values, (GDestroyNotify)g_bytes_unref);
+}
+
 int main(int argc, char *argv[]) {
   g_test_init(&argc, &argv, NULL);
 
@@ -56,6 +95,7 @@ int main(int argc, char *argv[]) {
   g_test_add_func("/fuzzer/string", static_string);
   g_test_add_func("/fuzzer/varstring", variable_string);
   g_test_add_func("/fuzzer/static_hex", static_hex);
+  g_test_add_func("/fuzzer/varblocksize", variable_block_size);
 
   return g_test_run();
 }
