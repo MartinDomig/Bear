d1fea87643235f808ee3415420d167a04c83fc0e
Martin Domig <martin.domig@wolfvision.net>
2024-07-28 14:30:57 +0200

    Add basic disconnection handling to audit tool

--- audit/wolfprot/wolfprot.c
+++ audit/wolfprot/wolfprot.c
@@ -2,14 +2,39 @@
 
 gboolean on_connect(BearFuzzer *fuzzer) {
   g_message("Connected");
-  // TODO send login data
+
+  // 09: Set value (0x01) + 2-byte command ID (0x08)
+  // 400B: command id for set login level
+  // 09: length of remaining payload
+  // 02: login level (0 = none, 1 = user, 2 = admin)
+  // The rest is the password "Password" (which is the default password).
+  const gchar *hex = "09 40 0B 09 02 50 61 73 73 77 6f 72 64";
+
+  g_autoptr(GBytes) login_data = bear_tools_hex_to_bytes(hex);
+  bear_fuzzer_send(fuzzer, login_data);
+
+  sleep(1); // give the device some time to process the login
+
   return TRUE;
 }
 
 gboolean on_disconnect(BearFuzzer *fuzzer) {
-  g_message("Disconnected @%s", bear_fuzzer_get_current_vector(fuzzer));
-  // TODO log vector and payload to file
-  return FALSE;
+  // We assume the remote side crashed if we get disconnected, so we log the offending payload to a file named after the
+  // vector.
+  const gchar *vector = bear_fuzzer_get_current_vector(fuzzer);
+  g_autoptr(GBytes) bytes = bear_fuzzer_get_data(fuzzer, vector);
+  g_autofree gchar *hexdump = bear_tools_bytes_to_hex(bytes);
+
+  g_autofree gchar *filename = g_strdup_printf("wp-disconnected-%s.txt", vector);
+  g_autoptr(GError) error = NULL;
+  g_file_set_contents(filename, hexdump, -1, &error);
+  if (error) {
+    g_warning("Failed to write crash data to %s: %s", filename, error->message);
+  }
+
+  g_message("Crash data written to %s", filename);
+
+  return TRUE;
 }
 
 gboolean on_receive(BearFuzzer *fuzzer, GBytes *data) {
